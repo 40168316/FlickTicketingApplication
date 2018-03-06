419b2f1a433bce0841da7b66981e04a8c01a5916
Calum <40168316@live.napier.ac.uk>
2018-03-06 09:13:15 +0000

    Initial Commit!
    IC

---
+++ FlickTicketingApp/FlickTicketingApp/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace FlickTicketingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addTicketButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.selectLabel = new System.Windows.Forms.Label();
+            this.concessionRadioButton = new System.Windows.Forms.RadioButton();
+            this.standardRadioButton = new System.Windows.Forms.RadioButton();
+            this.addLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.imaxCheckBox = new System.Windows.Forms.CheckBox();
+            this.realCheckBox = new System.Windows.Forms.CheckBox();
+            this.overallTotalLabel = new System.Windows.Forms.Label();
+            this.numberOfTicketsLabel = new System.Windows.Forms.Label();
+            this.noTicketsToPayForLabel = new System.Windows.Forms.Label();
+            this.noOfFreeTicketsLabel = new System.Windows.Forms.Label();
+            this.totalSavingsLabel = new System.Windows.Forms.Label();
+            this.totalButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // addTicketButton
+            // 
+            this.addTicketButton.Location = new System.Drawing.Point(309, 10);
+            this.addTicketButton.Name = "addTicketButton";
+            this.addTicketButton.Size = new System.Drawing.Size(130, 23);
+            this.addTicketButton.TabIndex = 1;
+            this.addTicketButton.Text = "Add Ticket";
+            this.addTicketButton.UseVisualStyleBackColor = true;
+            this.addTicketButton.Click += new System.EventHandler(this.addTicketButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.selectLabel);
+            this.panel1.Controls.Add(this.concessionRadioButton);
+            this.panel1.Controls.Add(this.standardRadioButton);
+            this.panel1.Location = new System.Drawing.Point(13, 9);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(159, 63);
+            this.panel1.TabIndex = 2;
+            // 
+            // selectLabel
+            // 
+            this.selectLabel.AutoSize = true;
+            this.selectLabel.Location = new System.Drawing.Point(0, 1);
+            this.selectLabel.Name = "selectLabel";
+            this.selectLabel.Size = new System.Drawing.Size(137, 13);
+            this.selectLabel.TabIndex = 3;
+            this.selectLabel.Text = "Please select a ticket type?";
+            // 
+            // concessionRadioButton
+            // 
+            this.concessionRadioButton.AutoSize = true;
+            this.concessionRadioButton.Location = new System.Drawing.Point(24, 41);
+            this.concessionRadioButton.Name = "concessionRadioButton";
+            this.concessionRadioButton.Size = new System.Drawing.Size(113, 17);
+            this.concessionRadioButton.TabIndex = 1;
+            this.concessionRadioButton.TabStop = true;
+            this.concessionRadioButton.Text = "Concession Ticket";
+            this.concessionRadioButton.UseVisualStyleBackColor = true;
+            this.concessionRadioButton.CheckedChanged += new System.EventHandler(this.concessionRadioButton_CheckedChanged);
+            // 
+            // standardRadioButton
+            // 
+            this.standardRadioButton.AutoSize = true;
+            this.standardRadioButton.Location = new System.Drawing.Point(24, 17);
+            this.standardRadioButton.Name = "standardRadioButton";
+            this.standardRadioButton.Size = new System.Drawing.Size(101, 17);
+            this.standardRadioButton.TabIndex = 0;
+            this.standardRadioButton.TabStop = true;
+            this.standardRadioButton.Text = "Standard Ticket";
+            this.standardRadioButton.UseVisualStyleBackColor = true;
+            this.standardRadioButton.CheckedChanged += new System.EventHandler(this.standardRadioButton_CheckedChanged);
+            // 
+            // addLabel
+            // 
+            this.addLabel.AutoSize = true;
+            this.addLabel.Location = new System.Drawing.Point(210, 9);
+            this.addLabel.Name = "addLabel";
+            this.addLabel.Size = new System.Drawing.Size(64, 13);
+            this.addLabel.TabIndex = 5;
+            this.addLabel.Text = "Add Extras?";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(13, 79);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(86, 13);
+            this.totalLabel.TabIndex = 6;
+            this.totalLabel.Text = "Running Total: £";
+            // 
+            // imaxCheckBox
+            // 
+            this.imaxCheckBox.AutoSize = true;
+            this.imaxCheckBox.Location = new System.Drawing.Point(213, 26);
+            this.imaxCheckBox.Name = "imaxCheckBox";
+            this.imaxCheckBox.Size = new System.Drawing.Size(52, 17);
+            this.imaxCheckBox.TabIndex = 7;
+            this.imaxCheckBox.Text = "IMAX";
+            this.imaxCheckBox.UseVisualStyleBackColor = true;
+            this.imaxCheckBox.CheckedChanged += new System.EventHandler(this.imaxCheckBox_CheckedChanged);
+            // 
+            // realCheckBox
+            // 
+            this.realCheckBox.AutoSize = true;
+            this.realCheckBox.Location = new System.Drawing.Point(213, 50);
+            this.realCheckBox.Name = "realCheckBox";
+            this.realCheckBox.Size = new System.Drawing.Size(62, 17);
+            this.realCheckBox.TabIndex = 8;
+            this.realCheckBox.Text = "Real3D";
+            this.realCheckBox.UseVisualStyleBackColor = true;
+            this.realCheckBox.CheckedChanged += new System.EventHandler(this.realCheckBox_CheckedChanged);
+            // 
+            // overallTotalLabel
+            // 
+            this.overallTotalLabel.AutoSize = true;
+            this.overallTotalLabel.Location = new System.Drawing.Point(173, 78);
+            this.overallTotalLabel.Name = "overallTotalLabel";
+            this.overallTotalLabel.Size = new System.Drawing.Size(79, 13);
+            this.overallTotalLabel.TabIndex = 9;
+            this.overallTotalLabel.Text = "Overall Total: £";
+            // 
+            // numberOfTicketsLabel
+            // 
+            this.numberOfTicketsLabel.AutoSize = true;
+            this.numberOfTicketsLabel.Location = new System.Drawing.Point(13, 96);
+            this.numberOfTicketsLabel.Name = "numberOfTicketsLabel";
+            this.numberOfTicketsLabel.Size = new System.Drawing.Size(97, 13);
+            this.numberOfTicketsLabel.TabIndex = 10;
+            this.numberOfTicketsLabel.Text = "Number of Tickets:";
+            // 
+            // noTicketsToPayForLabel
+            // 
+            this.noTicketsToPayForLabel.AutoSize = true;
+            this.noTicketsToPayForLabel.Location = new System.Drawing.Point(173, 96);
+            this.noTicketsToPayForLabel.Name = "noTicketsToPayForLabel";
+            this.noTicketsToPayForLabel.Size = new System.Drawing.Size(144, 13);
+            this.noTicketsToPayForLabel.TabIndex = 11;
+            this.noTicketsToPayForLabel.Text = "Number of Tickets to pay for:";
+            // 
+            // noOfFreeTicketsLabel
+            // 
+            this.noOfFreeTicketsLabel.AutoSize = true;
+            this.noOfFreeTicketsLabel.Location = new System.Drawing.Point(393, 96);
+            this.noOfFreeTicketsLabel.Name = "noOfFreeTicketsLabel";
+            this.noOfFreeTicketsLabel.Size = new System.Drawing.Size(98, 13);
+            this.noOfFreeTicketsLabel.TabIndex = 12;
+            this.noOfFreeTicketsLabel.Text = "Offer (free) Tickets:";
+            // 
+            // totalSavingsLabel
+            // 
+            this.totalSavingsLabel.AutoSize = true;
+            this.totalSavingsLabel.Location = new System.Drawing.Point(393, 79);
+            this.totalSavingsLabel.Name = "totalSavingsLabel";
+            this.totalSavingsLabel.Size = new System.Drawing.Size(79, 13);
+            this.totalSavingsLabel.TabIndex = 13;
+            this.totalSavingsLabel.Text = "Total Saving: £";
+            // 
+            // totalButton
+            // 
+            this.totalButton.Location = new System.Drawing.Point(309, 40);
+            this.totalButton.Name = "totalButton";
+            this.totalButton.Size = new System.Drawing.Size(130, 23);
+            this.totalButton.TabIndex = 14;
+            this.totalButton.Text = "Total and Pay";
+            this.totalButton.UseVisualStyleBackColor = true;
+            this.totalButton.Click += new System.EventHandler(this.totalButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 134);
+            this.Controls.Add(this.totalButton);
+            this.Controls.Add(this.totalSavingsLabel);
+            this.Controls.Add(this.noOfFreeTicketsLabel);
+            this.Controls.Add(this.noTicketsToPayForLabel);
+            this.Controls.Add(this.numberOfTicketsLabel);
+            this.Controls.Add(this.overallTotalLabel);
+            this.Controls.Add(this.realCheckBox);
+            this.Controls.Add(this.imaxCheckBox);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.addLabel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.addTicketButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button addTicketButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton concessionRadioButton;
+        private System.Windows.Forms.RadioButton standardRadioButton;
+        private System.Windows.Forms.Label selectLabel;
+        private System.Windows.Forms.Label addLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.CheckBox imaxCheckBox;
+        private System.Windows.Forms.CheckBox realCheckBox;
+        private System.Windows.Forms.Label overallTotalLabel;
+        private System.Windows.Forms.Label numberOfTicketsLabel;
+        private System.Windows.Forms.Label noTicketsToPayForLabel;
+        private System.Windows.Forms.Label noOfFreeTicketsLabel;
+        private System.Windows.Forms.Label totalSavingsLabel;
+        private System.Windows.Forms.Button totalButton;
+    }
+}
+
---
+++ FlickTicketingApp/FlickTicketingApp/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlickTicketingApp
+{
+    
+    public partial class Form1 : Form
+    {
+        // Variables used through the application
+        double runningTotal = 0;
+        double overallTotal = 0;
+        int numberOfTicketsAdded = 0;
+        int numberOfFreeTickets = 0;
+        int numberOfTicketsToPayFor = 0;
+        double moneySavedBasedOnOffers = 0;
+        bool ifConcessionHasJustBeenSelected = false;
+        bool ifStandardHasJustBeenSelected = false;
+        string currentDay = "Thursday";
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Update name of form
+            this.Text = "Flick Ticket Application";
+        }
+
+        // If Standard Radiobutton is checked
+        private void standardRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            // If standard radio button has been checked and concession radio button is not checked 
+            // (and statement not required as the radio buttons are in a panel, however, good practise)
+            if(standardRadioButton.Checked && !concessionRadioButton.Checked)
+            {
+                // Add £7.90 to the running total
+                updateRunningTotal(7.90);
+            }
+
+            // Set the bool for the standard radio button to true
+            ifStandardHasJustBeenSelected = true;
+
+            // If concession ticket type has previously been selected then
+            if (ifConcessionHasJustBeenSelected)
+            {
+                // Remove £5.40 from the running total
+                updateRunningTotal(-5.40);
+                // Reset boolean to false
+                ifConcessionHasJustBeenSelected = false;
+            }
+        }
+
+        // If Concession Radiobutton has been checked
+        private void concessionRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            // If standard radio button is not checked and concession radio button is checked 
+            if (!standardRadioButton.Checked && concessionRadioButton.Checked)
+            {
+                // Add £7.90 to the running total
+                updateRunningTotal(5.40);
+            }
+
+            // Set the bool for the concession radio button to true
+            ifConcessionHasJustBeenSelected = true;
+
+            // If standard ticket type has previously been selected then
+            if (ifStandardHasJustBeenSelected)
+            {
+                // Remove £-7.90 from the running total
+                updateRunningTotal(-7.90);
+                // Reset boolean to false               
+                ifStandardHasJustBeenSelected = false;
+            }
+        }
+
+        // Function which is used to add a value to the overall total - also updates the overall total label
+        private void updateOverallTotal(double value)
+        {
+            // Plus equal (runningTotal = runningTotal + value) the value passed in to the running total
+            overallTotal += value;
+            // Update the label wi
+            overallTotalLabel.Text = "Overall Total: £" + overallTotal.ToString();
+        }
+
+        // Function which is used to add a value to the running total - also updates the running total label
+        private void updateRunningTotal(double value)
+        {
+            // Plus equal (runningTotal = runningTotal + value) the value passed in to the running total
+            runningTotal += value;
+            // Update the label wi
+            totalLabel.Text = "Running Total: £" + runningTotal.ToString();
+        }
+
+        // If Add Ticket is pressed
+        private void addTicketButton_Click(object sender, EventArgs e)
+        {
+            // If neither Standard or Concession Radio Buttons have been pressed then produce error
+            if (!standardRadioButton.Checked && !concessionRadioButton.Checked)
+            {
+                MessageBox.Show("Please select a ticket type before continuing!", "Error");
+            }
+            // Else continue as normal
+            else
+            {
+                // If ticket type is standard
+                if (standardRadioButton.Checked)
+                {
+                    // Add £7.90 to the total
+                    updateOverallTotal(7.90);
+                }
+                // Else ticket type must be concession
+                else
+                {
+                    // Add £5.40 to the total
+                    updateOverallTotal(5.40);
+                }
+
+                // If extra IMAX is selected
+                if (imaxCheckBox.Checked)
+                {
+                    // Add £1.50 to the total
+                    updateOverallTotal(1.50);
+                }
+
+                // If extra Real3D is selected
+                if (realCheckBox.Checked)
+                {
+                    // Add £0.90 to the total
+                    updateOverallTotal(0.90);
+                }
+
+                // Add to totally ticket count
+                numberOfTicketsAdded += 1;
+                numberOfTicketsLabel.Text = "Number of Tickets: " + numberOfTicketsAdded.ToString();
+
+                // Check all current offers
+                // Only offer is three for one thursdays
+                if (currentDay == "Thursday")
+                {
+                    // Display messagebox notifying the user of the offer
+                    MessageBox.Show("As the current day is Thursday, you are applicable for the Three for One Thursday special offer. As you are due to pay for one ticket, you will get an additional two free.", "Special Offer");
+                    // Add and display number of free tickets due to the offer
+                    numberOfFreeTickets += 2;
+                    noOfFreeTicketsLabel.Text = "Offer (free) Tickets: " + numberOfFreeTickets.ToString();
+                    // Add and display total number of tickets
+                    numberOfTicketsAdded += 2;
+                    numberOfTicketsLabel.Text = "Number of Tickets: " + numberOfTicketsAdded.ToString();
+                    // Add and display the number of tickets to be paid for
+                    numberOfTicketsToPayFor += 1;
+                    noTicketsToPayForLabel.Text = "Number of Tickets to pay for: " + numberOfTicketsToPayFor.ToString();
+                    // Calculate the money saved based on the offer
+                    // If ticket type is standard
+                    if (standardRadioButton.Checked)
+                    {
+                        // Add the value to two standard tickets
+                        moneySavedBasedOnOffers += (7.90 * 2);
+                    }
+                    // Else ticket type must be concession
+                    else
+                    {
+                        // Add the value of two concession tickets
+                        moneySavedBasedOnOffers += (5.40 * 2);
+                    }
+                    // Update money saved label
+                    totalSavingsLabel.Text = "Total Savings: £" + moneySavedBasedOnOffers.ToString();
+
+                    // Clear current options in radio/check boxes
+                    concessionRadioButton.Checked = false;
+                    standardRadioButton.Checked = false;
+                    imaxCheckBox.Checked = false;
+                    realCheckBox.Checked = false;
+
+                    // Reset running total to zero
+                    runningTotal = 0.0;
+                    totalLabel.Text = "Running Total: £0.0";
+                }
+                // Reset important booleans as if not reset running total would go negative
+                ifStandardHasJustBeenSelected = false;
+                ifConcessionHasJustBeenSelected = false;
+            }
+        }
+
+        // If IMAX check box is pressed
+        private void imaxCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            // If IMAX check box is checked then
+            if(imaxCheckBox.Checked)
+            {
+                // Add £1.50 to the running total
+                updateRunningTotal(1.50);
+            }
+            // Else if IMAX check box is not checked then
+            else
+            {
+                // Remove £1.50 from the running total
+                updateRunningTotal(-1.50);
+            }
+        }
+
+        // If Real3D check box is checked
+        private void realCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            // If Real3D check box is checked then
+            if (realCheckBox.Checked)
+            {
+                // Add £0.90 to the running total
+                updateRunningTotal(0.90);
+            }
+            // Else if Real3D check box is not checked then
+            else
+            {
+                // Remove £0.90 from the running total
+                updateRunningTotal(-0.90);
+            }
+        }
+
+        // If total button is pressed
+        private void totalButton_Click(object sender, EventArgs e)
+        {
+            // If number of tickets to be paid for is less than one (ie 0)
+            if (numberOfTicketsToPayFor < 1)
+            {
+                // Messagebox explaining the user hasnt added a ticket and has nothing to pay for
+                MessageBox.Show("You have not yet added a ticket therefore have not been charged");
+            }
+            else
+            {
+                // Messagebox showing overall total, tickets to pay for, free tickets and saving
+                if (MessageBox.Show("Your final total is £" + overallTotal + ".You are required to pay for " + numberOfTicketsToPayFor + " tickets will gaining " + numberOfFreeTickets + " free tickets which saves you £" + moneySavedBasedOnOffers + "!", "Pay and Close", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    // If user pressed ok - exit application
+                    System.Windows.Forms.Application.Exit();
+                }
+            }
+        }
+    }
+}
